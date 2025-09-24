d80dedc6ea333b35e61e17a7644eab2833f8a946
Itaslab <r000050@teco.com.ar>
2025-09-24 16:38:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,12 @@ import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    
+
+@app.route('/')
+def home():
+    return "API JWT Token Generator funcionando correctamente"
+
+@app.route('/health')
+def health():
+    return {"status": "ok"}
+
